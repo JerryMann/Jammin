42426d4397eb49a8f40ffccdbaea03ce7d9ad796
JerryMann <herman.skurko@gmail.com>
2019-08-15 16:19:34 +0300

    added YourProfile activity with layout

---
+++ app/src/main/java/by/itacademy/pvt/jammin/mvp/YourProfileActivity.kt
@@ -0,0 +1,13 @@
+package by.itacademy.pvt.jammin.mvp
+
+import android.app.Activity
+import android.os.Bundle
+import by.itacademy.pvt.jammin.R
+
+class YourProfileActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_your_profile)
+
+    }
+}
